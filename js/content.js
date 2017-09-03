7b47c9e3bab602e67f5289721f9fdd08a848dd32
Trace Larson <trace.larson4917@gmail.com>
2017-09-03 14:33:16 -0500

    working on using Fetch API

--- js/content.js
+++ js/content.js
@@ -57,77 +57,78 @@ function GetMarvelData() {
     // console.log('opened request');
 
     // //-------Listen for onload event
-    // request.onload = function () {
-    //     //Check for success status codes
-    //     if (request.status >= 200 && request.status < 400) {
-    //         console.log("response succeeded");
-    //
-    //         //Parse data
-    //         var comicData = JSON.parse(request.responseText);
-    //         console.log("recieved data");
-    //
-    //         //Do something with the data
-    //         var imagePath = "";
-    //         var imageExtension = "";
-    //         var comicLink = "";
-    //         var comicTitle = "";
-    //         var resultsArray = comicData.data.results;
-    //
-    //         for (var i = 0; i < resultsArray.length; i++) {
-    //
-    //             //Get information from API results
-    //             imagePath = resultsArray[i].thumbnail.path;
-    //             imageExtension = resultsArray[i].thumbnail.extension;
-    //             comicLink = resultsArray[i].urls[0].url;
-    //             comicTitle = resultsArray[i].title;
-    //
-    //             //Generate child nodes of the results list
-    //             var liNode = document.createElement('li');
-    //
-    //             var aNode = document.createElement('a');
-    //
-    //             var imgNode = document.createElement('img');
-    //
-    //             var textNode = document.createTextNode(comicTitle);
-    //
-    //             aNode.appendChild(imgNode);
-    //             aNode.appendChild(textNode);
-    //             liNode.appendChild(aNode);
-    //             resultsList.appendChild(liNode);
-    //
-    //             //Add href and target attributes to anchor tags
-    //             resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('href', comicLink);
-    //             resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('target', '_blank');
-    //
-    //             //Add src attribute for each link
-    //             resultsList.getElementsByTagName('li')[i].getElementsByTagName('img')[0].setAttribute('src', BuildImagePath(imagePath, imageExtension));
+    //request.onload =
+    function processData (data) {
+        //Check for success status codes
+        //if (request.status >= 200 && request.status < 400) {
+         //   console.log("response succeeded");
 
-    //         }
-    //
-    //
-    //     } else {
-    //         //Catch response errors
-    //         console.log('response error', request);
-    //     }
-    // };
+            //Parse data
+            var comicData = data;//JSON.parse(request.responseText);
+            console.log("recieved data");
 
-    // //Listen for connection errors
-    // request.onerror = function () {
-    //     //Code for connection errors
-    //     console.log('connection errors');
-    // };
+            //Do something with the data
+            var imagePath = "";
+            var imageExtension = "";
+            var comicLink = "";
+            var comicTitle = "";
+            var resultsArray = comicData.data.results;
 
-    // //Send Request
-    // request.send();
-// }
+            for (var i = 0; i < resultsArray.length; i++) {
 
-// function BuildImagePath(path, ext) {
-//
-//     var fullImagePath = path + '/portrait_xlarge.' + ext;
-//     return fullImagePath;
-//
-// }
+                //Get information from API results
+                imagePath = resultsArray[i].thumbnail.path;
+                imageExtension = resultsArray[i].thumbnail.extension;
+                comicLink = resultsArray[i].urls[0].url;
+                comicTitle = resultsArray[i].title;
+
+                //Generate child nodes of the results list
+                var liNode = document.createElement('li');
+
+                var aNode = document.createElement('a');
+
+                var imgNode = document.createElement('img');
+
+                var textNode = document.createTextNode(comicTitle);
+
+                aNode.appendChild(imgNode);
+                aNode.appendChild(textNode);
+                liNode.appendChild(aNode);
+                resultsList.appendChild(liNode);
+
+                //Add href and target attributes to anchor tags
+                resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('href', comicLink);
+                resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('target', '_blank');
+
+                //Add src attribute for each link
+                resultsList.getElementsByTagName('li')[i].getElementsByTagName('img')[0].setAttribute('src', BuildImagePath(imagePath, imageExtension));
+
+            }
+
+
+        // } else {
+        //     //Catch response errors
+        //     console.log('response error', request);
+        // }
+    };
+
+    //Listen for connection errors
+    request.onerror = function () {
+        //Code for connection errors
+        console.log('connection errors');
+    };
+
+    //Send Request
+    request.send();
 }
+
+function BuildImagePath(path, ext) {
+
+    var fullImagePath = path + '/portrait_xlarge.' + ext;
+    return fullImagePath;
+
+};
+
 function ClearInputField(){
     searchName.value = '';
-}
+};
