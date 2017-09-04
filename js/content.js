3f953648e2d38e68ac6dd049487e3f82d710707e
Trace Larson <trace.larson4917@gmail.com>
2017-09-03 19:37:08 -0500

    editing color scheme, header image, and javascript issues.

--- js/content.js
+++ js/content.js
@@ -44,83 +44,57 @@ function GetMarvelData() {
 
     promise.then(function(response) {
         return response.json();
-    }).then(function(data) {
-        console.log(data);
-        return(data);
     }).then(processData).catch(function(error){console.log(error);});
 
 
-
-
-    //---------
-    // //Open request, ASYNC
-    // request.open('GET', url, true);
-    // console.log('opened request');
-
-    // //-------Listen for onload event
-    //request.onload =
     function processData (data) {
-        //Check for success status codes
-        //if (request.status >= 200 && request.status < 400) {
-         //   console.log("response succeeded");
 
-            //Parse data
-            var comicData = data;//JSON.parse(request.responseText);
-            console.log("recieved data");
+        //Parse data
+        var comicData = data;
+        console.log("recieved data");
 
-            //Do something with the data
-            var imagePath = "";
-            var imageExtension = "";
-            var comicLink = "";
-            var comicTitle = "";
-            var resultsArray = comicData.data.results;
+        //Do something with the data
+        var imagePath = "";
+        var imageExtension = "";
+        var comicLink = "";
+        var comicTitle = "";
+        var resultsArray = comicData.data.results;
 
-            for (var i = 0; i < resultsArray.length; i++) {
+        for (var i = 0; i < resultsArray.length; i++) {
 
-                //Get information from API results
-                imagePath = resultsArray[i].thumbnail.path;
-                imageExtension = resultsArray[i].thumbnail.extension;
-                comicLink = resultsArray[i].urls[0].url;
-                comicTitle = resultsArray[i].title;
+            //Get information from API results
+            imagePath = resultsArray[i].thumbnail.path;
+            imageExtension = resultsArray[i].thumbnail.extension;
+            comicLink = resultsArray[i].urls[0].url;
+            comicTitle = resultsArray[i].title;
 
-                //Generate child nodes of the results list
-                var liNode = document.createElement('li');
+            //Generate child nodes of the results list
+            var liNode = document.createElement('li');
 
-                var aNode = document.createElement('a');
+            var aNode = document.createElement('a');
 
-                var imgNode = document.createElement('img');
+            var imgNode = document.createElement('img');
 
-                var textNode = document.createTextNode(comicTitle);
+            var textNode = document.createTextNode(comicTitle);
 
-                aNode.appendChild(imgNode);
-                aNode.appendChild(textNode);
-                liNode.appendChild(aNode);
-                resultsList.appendChild(liNode);
+            aNode.appendChild(imgNode);
+            aNode.appendChild(textNode);
+            liNode.appendChild(aNode);
+            resultsList.appendChild(liNode);
 
-                //Add href and target attributes to anchor tags
-                resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('href', comicLink);
-                resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('target', '_blank');
+            //Add href and target attributes to anchor tags
+            resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('href', comicLink);
+            resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('target', '_blank');
 
-                //Add src attribute for each link
-                resultsList.getElementsByTagName('li')[i].getElementsByTagName('img')[0].setAttribute('src', BuildImagePath(imagePath, imageExtension));
+            //Add src attribute for each link
+            resultsList.getElementsByTagName('li')[i].getElementsByTagName('img')[0].setAttribute('src', BuildImagePath(imagePath, imageExtension));
+            //Add alt attribute for each link
+            resultsList.getElementsByTagName('li')[i].getElementsByTagName('img')[0].setAttribute('alt', comicTitle);
 
             }
 
-
-        // } else {
-        //     //Catch response errors
-        //     console.log('response error', request);
-        // }
     };
 
-    //Listen for connection errors
-    // request.onerror = function () {
-    //     //Code for connection errors
-    //     console.log('connection errors');
-    // };
-
-    // //Send Request
-    // request.send();
 }
 
 function BuildImagePath(path, ext) {
