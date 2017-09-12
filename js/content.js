2553f1a63ff5395a2f49edacba8af996e80c6409
Trace Larson <trace.larson4917@gmail.com>
2017-09-11 22:35:11 -0500

    Implemented WebStorageAPI to save users last search

--- js/content.js
+++ js/content.js
@@ -1,27 +1,3 @@
-//
-// window.addEventListener("resize", throttleResize);
-//
-//
-// var resizeTimeout;
-//
-// function throttleResize() {
-//
-//     if (!resizeTimeout) { // ignore if already running
-//         resizeTimeout = setTimeout( ()=>{
-//             resizeTimeout = null;
-//             handleResize();
-//         }, ) //delay time
-//     }
-// }
-// function handleResize() {
-//
-//     // code to run on resize
-//
-// }
-
-
-
-
 
 const searchButton = document.getElementById('searchButton');
 const resultsList = document.getElementById('results');
@@ -30,15 +6,26 @@ const searchName = document.getElementById('searchString');
 const searchTitle = document.getElementById('contentTitle');
 
 //Hide the content until the user searches
-content.setAttribute('class','hidden');
+content.setAttribute('class', 'hidden');
+
+//Event listener for page load, to check if localy stored data is availaible
+window.addEventListener('load', function() {
+    if(localStorage.getItem('userData')){
+        let localData = localStorage.getItem('userData');
+        content.removeAttribute('class');
+        searchTitle.innerHTML = 'Last Search';
+        processData(JSON.parse(localData));
+    }
+});
 
 //Enable Enter key to submit form.
-searchName.addEventListener('keypress', function(event){
+searchName.addEventListener('keypress', function (event) {
     if (event.keyCode == 13) {
         searchButton.click();
         event.preventDefault();
     }
 });
+
 //Event listeners for submitting the form, clearing the input field and the content, if present.
 searchButton.addEventListener('click', ClearContent);
 searchButton.addEventListener('click', GetMarvelData);
@@ -65,79 +52,82 @@ function GetMarvelData() {
     //Create Fetch request
     let promise = fetch(url);
     console.log(promise);
-
-    promise.then(function(response) {
+    promise.then(function (response) {
         return response.json();
-    }).then(processData).catch(function(error){
-
+    }).then(storeLocal).then(processData).catch(function (error) {
         console.log(error);
-
         let notFoundText = document.createTextNode("Marvel API response error");
+        let liNode = document.createElement('li');
+        liNode.appendChild(notFoundText);
+        resultsList.appendChild(liNode);
+    });
+}
+
+//store search locally.
+function storeLocal(data){
+    localStorage.setItem('userData', JSON.stringify(data));
+    return data;
+}
+
+function processData(data) {
+
+    //Parse data
+    let comicData = data;
+
+    //Store data locally.
+
+    console.log("recieved data");
+
+    //Do something with the data
+    let imagePath = "";
+    let imageExtension = "";
+    let comicLink = "";
+    let comicTitle = "";
+    let resultsArray = comicData.data.results;
+
+    if (resultsArray.length > 0) {
+
+        for (var i = 0; i < resultsArray.length; i++) {
+
+            //Get information from API results
+            imagePath = resultsArray[i].thumbnail.path;
+            imageExtension = resultsArray[i].thumbnail.extension;
+            comicLink = resultsArray[i].urls[0].url;
+            comicTitle = resultsArray[i].title;
+
+            //Generate child nodes of the results list
+            let liNode = document.createElement('li');
+            let aNode = document.createElement('a');
+            let imgNode = document.createElement('img');
+            let textNode = document.createTextNode(comicTitle);
+
+            aNode.appendChild(imgNode);
+            aNode.appendChild(textNode);
+            liNode.appendChild(aNode);
+            resultsList.appendChild(liNode);
+
+            //Add href and target attributes to anchor tags
+            resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('href', comicLink);
+            resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('target', '_blank');
+
+            //Add src attribute for each link
+            resultsList.getElementsByTagName('li')[i].getElementsByTagName('img')[0].setAttribute('src', BuildImagePath(imagePath, imageExtension));
+            //Add alt attribute for each link
+            resultsList.getElementsByTagName('li')[i].getElementsByTagName('img')[0].setAttribute('alt', comicTitle);
+
+        }
+    } else {
+        //populate content with an error notification that the comic could not be found.
+        let notFoundText = document.createTextNode("no comics found");
         let liNode = document.createElement('li');
 
         liNode.appendChild(notFoundText);
         resultsList.appendChild(liNode);
-    });
+    }
 
 
-    function processData (data) {
+};
 
-        //Parse data
-        let comicData = data;
-
-        console.log("recieved data");
-
-        //Do something with the data
-        let imagePath = "";
-        let imageExtension = "";
-        let comicLink = "";
-        let comicTitle = "";
-        let resultsArray = comicData.data.results;
-
-        if (resultsArray.length > 0) {
-
-            for (var i = 0; i < resultsArray.length; i++) {
-
-                //Get information from API results
-                imagePath = resultsArray[i].thumbnail.path;
-                imageExtension = resultsArray[i].thumbnail.extension;
-                comicLink = resultsArray[i].urls[0].url;
-                comicTitle = resultsArray[i].title;
-
-                //Generate child nodes of the results list
-                let liNode = document.createElement('li');
-                let aNode = document.createElement('a');
-                let imgNode = document.createElement('img');
-                let textNode = document.createTextNode(comicTitle);
-
-                aNode.appendChild(imgNode);
-                aNode.appendChild(textNode);
-                liNode.appendChild(aNode);
-                resultsList.appendChild(liNode);
-
-                //Add href and target attributes to anchor tags
-                resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('href', comicLink);
-                resultsList.getElementsByTagName('li')[i].getElementsByTagName('a')[0].setAttribute('target', '_blank');
-
-                //Add src attribute for each link
-                resultsList.getElementsByTagName('li')[i].getElementsByTagName('img')[0].setAttribute('src', BuildImagePath(imagePath, imageExtension));
-                //Add alt attribute for each link
-                resultsList.getElementsByTagName('li')[i].getElementsByTagName('img')[0].setAttribute('alt', comicTitle);
-
-            }
-        }else {
-            //populate content with an error notification that the comic could not be found.
-            let notFoundText = document.createTextNode("no comics found");
-            let liNode = document.createElement('li');
-
-            liNode.appendChild(notFoundText);
-            resultsList.appendChild(liNode);
-        }
-
-
-    };
-
-}
 
 //Build the image path using the path and extension.
 function BuildImagePath(path, ext) {
@@ -148,14 +138,15 @@ function BuildImagePath(path, ext) {
 }
 
 //Clear the input field.
-function ClearInputField(){
+function ClearInputField() {
     searchName.value = '';
 }
 
 //clears the current content of the page.
-function ClearContent(){
-    while (resultsList.firstChild){
+function ClearContent() {
+    while (resultsList.firstChild) {
         resultsList.removeChild(resultsList.firstChild);
-    };
+    }
+    ;
 
 }
